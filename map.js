e2ae47f17daa5942cf426e35c2e52d93c7145b31
Joanna Chien <joannachou713@gmail.com>
2021-04-05 20:29:27 +0800

    first commit

---
+++ map.js
@@ -0,0 +1,51 @@
+b1 = false
+    b2 = false
+    b3 = false
+    b4 = false
+    function aa(){
+      
+      p1 = document.getElementById("ccc")
+      if(b1==false){
+        p1.style.display = 'block'
+        b1 = true
+      }else{
+        p1.style.display = 'none'
+        b1 = false
+      }
+    }
+    function bb(){
+      
+      p2 = document.getElementById("ddd")
+      if(b2==false){
+        p2.style.display = 'block'
+        b2 = true
+      }else{
+        p2.style.display = 'none'
+        b2 = false
+      }
+    }
+    
+    function cc(){
+      
+      
+      p3 = document.getElementById('aaa')
+      
+      if(b3==false){
+        p3.style.display = 'block'
+        b3 = true
+      }else{
+        p3.style.display = 'none'
+        b3 = false
+      }
+    }
+    function dd(){
+      
+      p4 = document.getElementById('bbb')
+      if(b4==false){
+        p4.style.display = 'block'
+        b4 = true
+      }else{
+        p4.style.display = 'none'
+        b4 = false
+      }
+    }
